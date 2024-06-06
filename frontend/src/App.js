bc85e52ae325a7437bf9663bba1f380ac23bf34a
Ridham1502 <ridham74856@gmail.com>
2024-06-06 22:18:35 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/HomePage/HomePage';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar/>
+        <Routes> {/* Replace <Route> with <Routes> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<MovieDetails />} />
+          {/* <Route path="/events" element={<Events />} /> */}
+          {/* <Route path="/activities" element={<Activities />} /> */}
+          {/* <Route path="/sports" element={<Sports />} /> */}
+          {/* <Route path="/about" element={<AboutUs />} /> */}
+        </Routes> {/* Close <Routes> */}
+        <Footer/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
