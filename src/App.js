908445578a3f9554275eade9839bf91f4f5ca19b
Ridham1502 <ridham74856@gmail.com>
2024-06-06 17:22:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import RegisterLogin from './components/RegisterLogin/RegisterLogin';
-import LoginPage from './components/LoginPage/LoginPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+//import Home from './components/HomePage/HomePage';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import AdminPage from './components/AdminPage/AdminPage';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <div>
-        <Routes>
-          <Route path="/" element={<RegisterLogin />} />
+        <Navbar/>
+        <Routes> {/* Replace <Route> with <Routes> */}
+           <Route path="/" element={<RegisterLogin />} />
           <Route path="/login" element={<LoginPage />} />
-        </Routes>
+          // <Route path="/" element={<AdminPage />} />
+          <Route path="/movies" element={<MovieDetails />} />
+          {/* <Route path="/events" element={<Events />} /> */}
+          {/* <Route path="/activities" element={<Activities />} /> */}
+          {/* <Route path="/sports" element={<Sports />} /> */}
+           <Route path="/about" element={<AdminPage />} /> 
+        </Routes> {/* Close <Routes> */}
+        <Footer/>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
