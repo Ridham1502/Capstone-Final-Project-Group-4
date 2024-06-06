2f1c91b6cc8abd586526dd1d0e5670905582d842
pvuppu07 <vuppupratibha07@gmail.com>
2024-06-06 07:39:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import MovieDetails from './components/MovieDetails/MovieDetails';
-import AdminPage from './components/AdminPage/AdminPage';
+import './App.css';
+import RegisterLogin from './components/RegisterLogin/RegisterLogin';
+import LoginPage from './components/LoginPage/LoginPage';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
-        <Navbar/>
-        <Routes> {/* Replace <Route> with <Routes> */}
-          <Route path="/" element={<AdminPage />} />
-          <Route path="/movies" element={<MovieDetails />} />
-          {/* <Route path="/events" element={<Events />} /> */}
-          {/* <Route path="/activities" element={<Activities />} /> */}
-          {/* <Route path="/sports" element={<Sports />} /> */}
-          {/* <Route path="/about" element={<AboutUs />} /> */}
-        </Routes> {/* Close <Routes> */}
-        <Footer/>
+        <Routes>
+          <Route path="/" element={<RegisterLogin />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
