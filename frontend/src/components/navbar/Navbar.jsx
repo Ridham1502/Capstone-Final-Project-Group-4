f9f44cb06248bc617354d2e39cbe1f027bbfb732
Varalakshmi@07 <vuppupratibha07@gmail.com>
2024-07-04 23:40:38 -0400

    Updated Navbar
    Navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+import logo from '../Assets/logo.jpg'; // Ensure the path is correct
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-logo">
-        <img src="/logo.jpg" alt="Logo" />
+        <Link to="/"><img src={logo} alt="Cultural Canvas Logo" /></Link>
       </div>
       <div className="navbar-links">
-        <a href="#movies">Movies</a>
-        <a href="#events">Events</a>
-        <a href="#activities">Activities</a>
-        <a href="#sports">Sports</a>
-        <a href="#about">About Us</a>
+        <Link to="/">Home</Link>
+        <Link to="/movielist">Movie List</Link>
+        <Link to="/addmovie">Add Movies/Events</Link>
+       
+        <Link to="/activities">Activities</Link>
+        <Link to="/sports">Sports</Link>
+        <Link to="/about">About Us</Link>
         <div className="dropdown">
           <button className="dropbtn">Canada</button>
           <div className="dropdown-content">
-            <a href="#toronto">Toronto</a>
-            <a href="#vancouver">Vancouver</a>
-            <a href="#montreal">Montreal</a>
+            <Link to="/toronto">Toronto</Link>
+            <Link to="/vancouver">Vancouver</Link>
+            <Link to="/montreal">Montreal</Link>
           </div>
         </div>
-        <button className="sign-in-btn">Sign In</button>
+        <button className="sign-in-button" onClick={() => window.location.href='/login'}>Sign In</button>
+        <button className="sign-in-button" onClick={() => window.location.href='/register'}>Register</button>
+       
       </div>
     </div>
-  )
+  );
 }
