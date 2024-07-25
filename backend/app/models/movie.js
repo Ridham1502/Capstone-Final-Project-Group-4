9a4bdec10668d33630ab43619ff0a402c44bc9c1
pvuppu07 <vuppupratibha07@gmail.com>
2024-07-25 10:52:02 -0400

    updated movie.js

--- backend/app/models/movie.js
+++ backend/app/models/movie.js
@@ -1,7 +1,33 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// // Define Movie Schema
+// const movieSchema = new Schema({
+//     image: { type: String, required: true },
+//     title: { type: String, required: true },
+//     cast: { type: String, required: true },
+//     director: { type: String, required: true },
+//     genre: { type: String, required: true },
+//     language: { type: String, required: true },
+//     releaseDate: { type: Date, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     category: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// const Movie = mongoose.model('Movie', movieSchema);
+
+// module.exports = Movie;
+
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define Movie Schema
+
 const movieSchema = new Schema({
     image: { type: String, required: true },
     title: { type: String, required: true },
