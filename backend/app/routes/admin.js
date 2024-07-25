908136d95c45e15055b72d782917caefabd5c419
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-07-25 01:08:00 -0400

    admin.js
    modified admin.js in routes folder

--- backend/app/routes/admin.js
+++ backend/app/routes/admin.js
@@ -3,12 +3,26 @@ const router = express.Router()
 const controller = require('../controllers/admin');
 
 
+router.get(
+    '/addAdmin',
+    controller.addAdmin
+)
+
+router.post(
+    '/login',
+    controller.login
+)
 
 router.post(
     '/createMovie',
     controller.createMovie
 )
 
+
+router.get(
+    '/Movie',
+    controller.Movie
+)
 
 router.get(
     '/listMovie',
@@ -21,5 +35,24 @@ router.get(
     controller.listEvents
 )
 
+router.get(
+    '/listAllMovieAndEvent',
+    controller.listAllMovieAndEvent
+)
+
+router.patch(
+    '/editMovieDetails',
+    controller.editMovieDetails 
+)
+
+router.get(
+    '/MovieDetails',
+    controller.MovieDetails 
+)
+
+router.delete(
+    '/movieDeleteByid',
+    controller.movieDeleteByid 
+)
 
 module.exports = router
