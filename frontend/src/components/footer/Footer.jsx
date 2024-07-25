f7171d17a86dc8fc6d25d46592826dc52a3e1e1b
pvuppu07 <vuppupratibha07@gmail.com>
2024-07-25 11:04:10 -0400

    updated Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,43 +1,40 @@
 import React from 'react';
-import { FaInstagram, FaTwitter, FaLinkedin, FaYoutube } from 'react-icons/fa';
 import './Footer.css';
+import { FaInstagram, FaTwitter, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import logo from '../Assets/logo.jpg';
 
-export default function Footer() {
+const Footer = () => {
   return (
     <div className="footer-bg-light">
       <div className="footer-container">
-        <div className="footer-section">
-          <ul className="list-unstyled">
-            <p className="footer-title">
-              <img src="/logo.jpg" alt="Logo" className="footer-logo" />
-            </p>
-            <div className="social-icons">
-              <FaInstagram className="icon instagram" />
-              <FaTwitter className="icon twitter" />
-              <FaLinkedin className="icon linkedin" />
-              <FaYoutube className="icon youtube" />
-            </div>
-          </ul>
+        <div className="footer-section footer-logo">
+          <img src={logo} alt="Cultural Canvas Logo" />
+          <div className="social-icons">
+            <FaInstagram className="icon instagram" />
+            <FaTwitter className="icon twitter" />
+            <FaLinkedin className="icon linkedin" />
+            <FaYoutube className="icon youtube" />
+          </div>
         </div>
         <div className="footer-section">
+          <h4 className="footer-title">Movies</h4>
           <ul className="list-unstyled">
-            <p className="footer-subtitle">Movies</p>
             <li className="footer-link">Features</li>
             <li className="footer-link">Live Share</li>
             <li className="footer-link">Video Record</li>
           </ul>
         </div>
         <div className="footer-section">
+          <h4 className="footer-title">Community</h4>
           <ul className="list-unstyled">
-            <p className="footer-subtitle">Community</p>
             <li className="footer-link">Featured Artists</li>
             <li className="footer-link">The Portal</li>
             <li className="footer-link">Live Events</li>
           </ul>
         </div>
         <div className="footer-section">
+          <h4 className="footer-title">Company</h4>
           <ul className="list-unstyled">
-            <p className="footer-subtitle">Company</p>
             <li className="footer-link">About Us</li>
             <li className="footer-link">Contact Us</li>
             <li className="footer-link">History</li>
@@ -46,4 +43,6 @@ export default function Footer() {
       </div>
     </div>
   );
-}
+};
+
+export default Footer;
