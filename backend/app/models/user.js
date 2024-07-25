9376956738e9dda0737449644dd5254f31854d05
Ridham1502 <ridham74856@gmail.com>
2024-07-26 02:13:13 +0530

    Update user.js

--- backend/app/models/user.js
+++ backend/app/models/user.js
@@ -80,28 +80,36 @@ const UserSchema = new mongoose.Schema(
 );
 
 const hash = (user, salt, next) => {
+
+
     bcrypt.hash(user.password, salt, null, (error, newHash) => {
+
+
         if (error) {
             return next(error);
+
+
         }
+        
         user.password = newHash;
         return next();
     });
 };
 
-//const genSalt = (user, SALT_FACTOR, next) => {
-//    bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
-//        if (err) {
-//            return next(err);
-//        }
-//        return hash(user, salt, next);
-//    });
-//};
-
 const genSalt = (user, SALT_FACTOR, next) => {
-    bcrypt.genSalt(SALT_FACTOR)
-        .then(salt => hash(user, salt, next))
-        .catch(err => next(err));
+
+
+    bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+        if (err) {
+
+
+            return next(err);
+
+        }
+
+
+        return hash(user, salt, next);
+    });
 };
 
 UserSchema.pre("save", function (next) {
