afe27ddf77a07ceb33bf7fb149790efe443ff103
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-08-14 12:19:00 -0400

    removed unnecessary comments from api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5002/', // Replace with your API base URL
-  timeout: 10000, // Adjust timeout as needed
+  baseURL: 'http://localhost:5002/',
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
