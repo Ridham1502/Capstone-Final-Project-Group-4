c348413aa07ef062408f9e34ff9adba5903b53c9
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-08-14 10:44:18 -0400

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -13,24 +13,27 @@ const movieRoutes = require('./app/routes/movie');
 const eventRoutes = require('./app/routes/event');
 const theaterRoutes = require('./app/routes/theaters'); 
 
+var fs = require("fs");
 const app = express();
 
 app.use(helmet());
 
 const corsOptions = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204,
-  "allowedHeaders": 'Content-Type, Authorization'
-};
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "allowedHeaders": 'Content-Type, Authorization'
+}
 
 app.use(cors());
 app.options('*', cors(corsOptions));
 let server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(":method :url :status :response-time ms - :res[content-length]"));
+app.use(
+    morgan(":method :url :status :response-time ms - :res[content-length]")
+);
 
 app.use(passport.initialize());
 app.use(fileUpload());
@@ -39,30 +42,34 @@ app.use('/user', userRoutes); // User routes
 app.use('/admin', adminRoutes); // Admin routes
 app.use('/api', movieRoutes); // Movie routes
 app.use('/api', eventRoutes); // Event routes
-app.use('/api/theaters', theaterRoutes); // Ensure this line is correct
+app.use('/api/theaters', theaterRoutes); 
 
 app.use((req, res, next) => {
-  const error = {
-    message: "Route not found",
-    status: 404,
-    timestamp: new Date(),
-  };
-  res.status(404).json({ error });
+    const error = {
+        message: "Route not found",
+        status: 404,
+        timestamp: new Date(),
+    };
+    res.status(404).json({ error });
 });
 
+
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  const status = err.status || 500;
-  res.status(status).json({ error: err.message || "Internal Server Error" });
+    console.error(err.stack);
+    const status = err.status || 500;
+    res.status(status).json({ error: err.message || "Internal Server Error" });
 });
 
 server.listen(process.env.PORT || 5002, () => {
-  console.log("****************************");
-  console.log(`*    Starting HTTP Server`);
-  console.log(`*    Port: ${process.env.PORT || 5002}`);
-  console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`);
-  console.log(`*    Database: MongoDB`);
-  console.log(`*    DB Connection: OK\n****************************\n`);
+    console.log("****************************");
+    console.log(
+        `*    Starting ${process.env.ENV === "local" ? "HTTP" : "HTTPS"
+        } Server`
+    );
+    console.log(`*    Port: ${process.env.PORT || 5002}`);
+    console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`);
+    console.log(`*    Database: MongoDB`);
+    console.log(`*    DB Connection: OK\n****************************\n`);
 });
 
-initMongo();
+initMongo()
