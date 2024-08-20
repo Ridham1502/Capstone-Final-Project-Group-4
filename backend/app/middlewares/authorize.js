8cda1262ff80ced276c035aa48d1b59c597c0e25
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-08-20 17:58:37 -0400

    Add authorize.js middleware

---
+++ backend/app/middlewares/authorize.js
@@ -0,0 +1,12 @@
+function authorize(role) {
+ 
+    return (req, res, next) => {
+        if (req.user.role === role) {
+            next();
+        } else {
+            res.status(403).json({ message: 'You not have access for this' });
+        }
+    };
+}
+
+module.exports = authorize;
