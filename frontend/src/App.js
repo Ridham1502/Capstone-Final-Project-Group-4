faaf7c9374dd2376f5e3e1afc184d2a6d0776d8b
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-08-20 16:03:34 -0400

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,17 +24,13 @@ import ContactUsPage from './components/ContactUsPage/ContactUsPage';
 import FAQPage from './components/FAQ/FAQPage';
 
 
-
-
 function App() {
   return (
     <Router>
       <div>
         <MainNavigation />
         <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactUsPage />} />
-
+          <Route path="/" element={<HomePage />} />
           <Route path="/movie" element={<MovieList />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/confirmation" element={<Confirmation />} />
@@ -44,7 +40,8 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/loginadmin" element={<AdminLogin />} />
           <Route path="/moviedetails" element={<MovieDetails />} />
-          <Route path="/bookticket" element={<BookTicket />} /> 
+          <Route path="/bookticket" element={<BookTicket />} />
+          <Route path="/contact" element={<ContactUsPage />} />
           <Route path="/faqpage" element={<FAQPage />} />
           <Route path="/admin/*" element={<AdminDashboard />}>
             <Route index element={<h3>Welcome to Admin Dashboard</h3>} />
@@ -53,9 +50,8 @@ function App() {
             <Route path="foodadd" element={<FoodAdd />} />
             <Route path="bookinglist" element={<Booking />} />
             <Route path="foodlist" element={<FoodList />} />
-          
-
           </Route>
+        
         </Routes>
         <FooterConditional />
         <Toaster />
